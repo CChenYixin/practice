e8a85f4a6f48d4a4ed19714d8d322144a4e3e338
chenyixin <onepiececee@163.com>
2017-01-13 13:24:46 +0800

    demo
    demo

--- practice_14/demo.js
+++ practice_14/demo.js
@@ -7,6 +7,7 @@ var aqiData = [
     ["西安", 100]
 ];
 
+
 (function() {
 
     /*
@@ -14,12 +15,6 @@ var aqiData = [
     遍历读取aqiData中各个城市的数据
     将空气质量指数大于60的城市显示到aqi-list的列表中
     */
-    // for (var i = 0; i < aqiData.length; i++) {
-    //     if (aqiData[i][1] > 60) {
-    //         document.write(aqiData[i][0] + "," + aqiData[i][1] + "<br/>");
-    //     }
-    // }
-
 
     //用sort方法排序
     aqiData.sort(function (a,b){
@@ -27,13 +22,16 @@ var aqiData = [
     });
 
     var hanZi = ["一","二","三","四","五","六","七","八","九","十"];
+    var oUl = document.getElementById("aqi-list");
+    oUl.innerHTML = "";
 
     //遍历并添加到ul中
     for(var i=0;i<aqiData.length;i++){
       if(aqiData[i][1] > 60){
         var li = document.createElement("li");
-        li.appendChild(document.createTextNode("第" + hanZi[i] + "名：" + aqiData[i][0] + "，" + aqiData[i][1]));
-        document.getElementById("aqi-list").appendChild(li);
+        li.innerHTML = "第" + hanZi[i] + "名:" + aqiData[i][0] + "," + aqiData[i][1];
+        oUl.appendChild(li);
       }
     }
+
 })();
