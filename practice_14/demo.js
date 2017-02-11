b4a05cee8d029ee31826ff31182718a4f081734b
chenyixin <onepiececee@163.com>
2017-02-11 20:16:21 +0800

    复写
    复写

--- practice_14/demo.js
+++ practice_14/demo.js
@@ -1,4 +1,45 @@
-var aqiData = [
+// var aqiData = [
+//     ["北京", 90],
+//     ["上海", 50],
+//     ["福州", 10],
+//     ["广州", 50],
+//     ["成都", 90],
+//     ["西安", 100]
+// ];
+//
+//
+// (function() {
+//
+//     /*
+//     在注释下方编写代码
+//     遍历读取aqiData中各个城市的数据
+//     将空气质量指数大于60的城市显示到aqi-list的列表中
+//     */
+//
+//     //用sort方法排序
+//     aqiData.sort(function (a,b){
+//       return b[1] - a[1];
+//     });
+//
+//     var hanZi = ["一","二","三","四","五","六","七","八","九","十"];
+//     var oUl = document.getElementById("aqi-list");
+//     oUl.innerHTML = "";
+//
+//     //遍历并添加到ul中
+//     for(var i=0;i<aqiData.length;i++){
+//       if(aqiData[i][1] > 60){
+//         var li = document.createElement("li");
+//         li.innerHTML = "第" + hanZi[i] + "名:" + aqiData[i][0] + "," + aqiData[i][1];
+//         oUl.appendChild(li);
+//       }
+//     }
+//
+// })();
+
+//复写
+
+//数据
+var aqiData =[
     ["北京", 90],
     ["上海", 50],
     ["福州", 10],
@@ -7,31 +48,24 @@ var aqiData = [
     ["西安", 100]
 ];
 
-
-(function() {
-
-    /*
-    在注释下方编写代码
-    遍历读取aqiData中各个城市的数据
-    将空气质量指数大于60的城市显示到aqi-list的列表中
-    */
-
-    //用sort方法排序
-    aqiData.sort(function (a,b){
-      return b[1] - a[1];
+//函数
+(function(){
+    //sort排序
+    aqiData.sort(function(a,b){
+        return b[1] - a[1];
     });
 
-    var hanZi = ["一","二","三","四","五","六","七","八","九","十"];
-    var oUl = document.getElementById("aqi-list");
-    oUl.innerHTML = "";
+    var hanZi = ['一','二','三','四','五','六','七','八','九','十'];
+    var oUl = document.getElementById('aqi-list');
+        oUl.innerHTML ="";
 
-    //遍历并添加到ul中
+    //遍历并展示
     for(var i=0;i<aqiData.length;i++){
-      if(aqiData[i][1] > 60){
-        var li = document.createElement("li");
-        li.innerHTML = "第" + hanZi[i] + "名:" + aqiData[i][0] + "," + aqiData[i][1];
-        oUl.appendChild(li);
-      }
+        //过滤空气质量大于60的城市
+        if(aqiData[i][1]>60){
+            var li =document.createElement('li');
+            li.innerHTML = "第" + hanZi[i] + "名:" + aqiData[i][0]+","+aqiData[i][1];
+            oUl.appendChild(li);
+        }
     }
-
 })();
--- practice_15/demo.js
+++ practice_15/demo.js
@@ -5,7 +5,7 @@
  */
 
 
-function getData() {
+/*function getData() {
     var oUl = document.getElementById('source');
     var data = [];
 
@@ -16,6 +16,21 @@ function getData() {
         data.push([city,num]);
     }
     return data;
+}*/
+//复写
+
+function getData(){
+    var oUl = document.getElementById('source');
+    var data = [];
+
+    for(var i=0;i<oUl.childElementCount;i++){
+        var oLi = oUl.children[i];
+        var city = oLi.innerHTML.split('空气质量')[0];
+        var num = Number(oLi.children[0].innerHTML);
+
+        data.push([city,num]);
+    }
+    return data;
 }
 
 /**
@@ -23,9 +38,16 @@ function getData() {
  * 按空气质量对data进行从小到大的排序
  * 返回一个排序后的数组
  */
-function sortAqiData(data) {
+/*function sortAqiData(data) {
     data.sort(function(a,b){
         return a[1] - b[1];
+    });
+    return data;
+}*/
+
+function sortAqiData(data){
+    data.sort(function(a,b){
+        return a[1] -b[1];
     });
     return data;
 }
@@ -35,7 +57,7 @@ function sortAqiData(data) {
  * 将排好序的城市及空气质量指数，输出显示到id位resort的列表中
  * 格式见ul中的注释的部分
  */
-function render(data) {
+/*function render(data) {
     var ulList = document.getElementById('resort');
     var cnum = ["一","二","三","四","五","六","七","八","九","十"];
     for(var i= 0;i<data.length;i++){
@@ -43,17 +65,39 @@ function render(data) {
         li.innerHTML = '第' + cnum[i] + '名：' + data[i][0] + "空气质量<b>" + data[i][1] +"</b>";
         ulList.appendChild(li);
     }
+}*/
+function render(data){
+    var ulList = document.getElementById('resort');
+    var cnum = ['一','二','三','四','五','六','七','八','九','十'];
+
+    for(var i=0;i<data.length;i++){
+        var li = document.createElement('li');
+        li.innerHTML = '第' + cnum[i] +'名：' + data[i][0] +'空气质量<b>' +data[i][1]+'</b>';
+        ulList.appendChild(li);
+    }
 }
 
-function btnHandle() {
+/*function btnHandle() {
     var aqiData = getData();
     aqiData = sortAqiData(aqiData);
     render(aqiData);
+}*/
+
+function btnHandle(){
+    var aqiData = getData();
+    aqiData = sortAqiData(aqiData);
+    render(aqiData);
+
 }
 
-
-function init() {
+/*function init() {
     var btn = document.getElementById("sort-btn");
+    btn.onclick = btnHandle;
+}
+init();*/
+
+function init(){
+    var btn = document.getElementById('sort-btn');
     btn.onclick = btnHandle;
 }
 init();
