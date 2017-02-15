a527253394c437c7bd6ee26c7abdf0a883c4e6e8
chenyixin <onepiececee@163.com>
2017-02-15 14:14:11 +0800

    修改
    修改

--- practice_21/demo.js
+++ practice_21/demo.js
@@ -23,60 +23,42 @@ var arrTag = [],
 //初始化函数
 
 //输入
-/*function displayTag(){
+function displayTag(){
     var tag =  $('tag_input').value.trim();
     arrTag.push(tag);
     renderTag();
 }
-*/
-function displayHobby(){
+
+function displayHobby(e){
     var hobby = $('hobby_input').value.trim();
     arrHobby.push(hobby);
     renderHobby();
+    e.preventDefault();
+    e.stopPropagation();
 }
-
-/*//鼠标悬停增加删除
-function addDelete(){
-    var span = $(id);
-    span.innerHTML = '点击删除' +html;
-    span.style.background = '#000';
-    span.style.color = '#fff';
-}
-//鼠标点击删除
-function clickDelete(arr){
-    if(arr.length === ''){
-        alert('没有可供删除的选项！');
-    }
-    var del = confirm('确认要删除这个选项吗' + arr[arr.length-1] + '吗？');
-    if(del){
-        arr.pop();
-        render();
-    }
-}*/
-
 
 //添加事件
 
-function clickKeyup(){
+function clickKeyup() {
     addEvent(hobbyBtn,'click',displayHobby);
+    addEvent($('tag_input'),'keyup',function(event){
+        event = event || window.event;
+        if(event.keyCode === 13){
+            displayTag();
+        }
+    });
 }
 
-/*$('tag_input').onkeyup = function(event){
-    event = event || window.event;
-    if(event.keyCode === 13){
-        handler();
-    }
-};*/
 //渲染
 
-/*function renderTag(){
+function renderTag(){
     var tagList='';
     for(var item in arrTag){
         tagList += '<span class="numClass">' + arrTag[item] +'</span>';
     }
     $('tag_list').innerHTML = tagList;
 }
-*/
+
 function renderHobby(){
     var hobbyList='';
     for(var item in arrHobby){
@@ -88,7 +70,7 @@ function renderHobby(){
 //初始化
 function init(){
     clickKeyup();
-    // renderTag();
+    renderTag();
     renderHobby();
 }
 init();
--- practice_21/demo1.js
+++ practice_21/demo1.js
@@ -17,10 +17,11 @@ function addEvent(element,type,handler){
 
 var arrHobby=[];
 
-function displayHobby(){
-    var hobby = $('textarea').value.trim();
+function displayHobby(e){
+    var hobby = $('textarea').value.trim().split(/[,，、\s]/);
     arrHobby.push(hobby);
-    renderHobby();
+    renderHobby(e);
+    e.preventDefault();
 }
 
 
@@ -41,3 +42,72 @@ function init(){
     renderHobby();
 }
 init();
+/*var $ = function(x){
+  return document.querySelector(x);
+};
+var $a = function(id){
+    return document.getElementById(id);
+};
+
+function addEvent(element,type,handler){
+    if(element.addEventListener){
+        element.addEventListener(type,handler,false);
+    }else if (element.attachEvent) {
+        element.attachEvent('on'+type,handler);
+    }else{
+        element['on'+type] = handler;
+    }
+}
+
+var arrHobby=[],
+    retHobby=[];
+
+
+//获取textarea的值
+function getHobbys(){
+    var text = $('textarea').value.trim();
+    if(text === ''){
+        alert('输入值不能为空');
+    }
+    append = text.split(/[,，\s]+/);
+    arrHobby.push(append);
+    return arrHobby;
+}
+
+function unique(e){
+    for(var i=0; i<arrHobby.length;i++){
+        var item = arrHobby[i];
+        if(retHobby.indexOf(item) === -1){
+            retHobby.push(item);
+        }
+    }
+    return retHobby;
+}
+
+
+function displayHobby(e){
+
+    renderHobby();
+    e.preventDefault();
+}
+
+
+function clickKeyup(){
+    addEvent($('button'),'click',displayHobby);
+}
+
+function renderHobby(){
+    var hobbyList='';
+    for(var item in retHobby){
+        hobbyList += '<span class="numClass">' + retHobby[item] +'</span>';
+    }
+    $a('hobby_list').innerHTML = hobbyList;
+
+}
+
+function init(){
+    clickKeyup();
+    renderHobby();
+}
+init();
+*/
